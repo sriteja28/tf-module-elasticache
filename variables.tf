9982281f02e255a2ff1a32a7bff83c15fd333252
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-04 12:40:05 +0530

    Terraform

--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "tags" {}
 variable "subnet_ids" {}
 variable "sg_subnet_cidr" {}
 variable "kms_key_arn" {}
+variable "engine" {}
+variable "node_type" {}
+variable "engine_version" {}
+variable "port" {
+  default = 6379
+}
+variable "vpc_id" {}
+variable "num_node_groups" {}
+variable "replicas_per_node_group" {}
